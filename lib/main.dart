f4c6ea4d0f78f33f4650881e49a12764a04cd929
hishamkame <hishamkamel370@gmail.com>
2024-06-29 20:53:40 +0300

    create font family for app

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,12 @@ class QuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      theme: ThemeData(
+        fontFamily: 'Montserrat',
+      ),
+      home: const SplashPage(),
     );
   }
 }
