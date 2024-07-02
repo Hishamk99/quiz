64a1051ff0ed4b0bf244005299ae964ca0b7606c
hishamkame <hishamkamel370@gmail.com>
2024-07-02 09:53:03 +0300

    create quetions list

---
+++ lib/features/quiz/data/questions_list.dart
@@ -0,0 +1,45 @@
+import 'package:quiz_app/features/quiz/models/quiz_model.dart';
+
+class QuestionList {
+  static List<QuizModel> questionList = [
+    QuizModel(
+      question: "what is your job as engineer ",
+      correctAnswer: '0',
+      answers: [
+        "flutter developer",
+        "doctor",
+        "teacher",
+      ],
+    ),
+    QuizModel(
+      question: "what is make flutter ",
+      correctAnswer: '2',
+      answers: [
+        "microsoft",
+        "oracle",
+        "google",
+        "amazon",
+      ],
+    ),
+    QuizModel(
+      question: "What is the capital of France?",
+      correctAnswer: '1',
+      answers: [
+        "Berlin",
+        "Paris",
+        "London"
+            "Rome",
+      ],
+    ),
+    QuizModel(
+      question: "Who wrote the play \"Romeo and Juliet\"",
+      correctAnswer: '0',
+      answers: [
+        "William Shakespeare",
+        "me",
+        "Charles",
+        "Hisham",
+      ],
+    ),
+  ];
+}
